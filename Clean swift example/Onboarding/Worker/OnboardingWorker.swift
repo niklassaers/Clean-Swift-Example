32fe7d56e232c54c4a64f5b5a52dbe8c060d9065
Niklas Saers <niklas@saers.com>
2020-05-24 13:51:07 +0200

    Add a bit of worker example

--- Clean swift example/Onboarding/Worker/OnboardingWorker.swift
+++ Clean swift example/Onboarding/Worker/OnboardingWorker.swift
@@ -2,9 +2,26 @@ import Foundation
 
 final class OnboardingWorker: OnboardingWorkerProtocol {
     
+    static let kOnboardingComplete = "onboarding-complete"
+    
     lazy var userDefaults: UserDefaultsProtocol = UserDefaults.standard
     
+    private func registerDefaults(_ userDefaults: UserDefaultsProtocol) {
+        userDefaults.register(defaults: [
+            OnboardingWorker.kOnboardingComplete: false
+        ])
+    }
+    
+    var isOnboardingComplete: Bool {
+        get {
+            
+            registerDefaults(userDefaults)
+            return userDefaults.bool(forKey: OnboardingWorker.kOnboardingComplete)
+        }
+    }
+    
     func completeOnboarding() {
-        userDefaults.set(true, forKey: "onboarding-complete")
+        
+        userDefaults.set(true, forKey: OnboardingWorker.kOnboardingComplete)
     }
 }
